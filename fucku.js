20d28172023ce2d58e78056a50d80203884e91ee
Y.C.Huang <33322926+ShiriNmi1520@users.noreply.github.com>
2020-07-22 19:24:26 +0800

    Update fucku.js

--- fucku.js
+++ fucku.js
@@ -1,7 +1,7 @@
 var obj = { "appid":"abo","encryptkey":"123qWe" };
 $.ajax({
 	type: 'POST',
-	url: 'http://webapi.isdc.io:2000/api/auths/apilogin',
+	url: 'https://webapi.isdc.io/api/auths/apilogin',
 	data: obj,
 	success: function(data) {
 		console.log(data);
