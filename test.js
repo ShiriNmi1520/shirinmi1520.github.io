61609898c19cfc457e7e9b62ef58804862155dec
Y.C.Huang <33322926+ShiriNmi1520@users.noreply.github.com>
2021-11-12 22:06:47 +0800

    add resume

--- test.js
+++
@@ -1,9 +0,0 @@
-var obj = { "appid":"abo","encryptkey":"123qWe" };
-$.ajax({
-	type: 'POST',
-	url: 'https://webapi.isdc.io/api/auths/apilogin',
-	data: obj,
-	success: function(data) {
-		console.log(data);
-		}
-	})
